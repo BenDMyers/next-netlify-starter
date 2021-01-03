da5738fd6b3f0e0e3252101643a353631be937f9
Ben Myers <ben.myers@okstate.edu>
2021-01-02 18:14:28 -0600

    Create netlifyAuth export

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,52 @@
+import {useEffect, useState} from 'react';
+import netlifyAuth from '../netlifyAuth';
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+	let [user, setUser] = useState(null);
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+	useEffect(() => {
+		netlifyAuth.initialize((user) => {
+			setLoggedIn(!!user);
+		});
+	});
 
-      <Footer />
-    </div>
-  )
+	let login = () => {
+		netlifyAuth.authenticate((user) => {
+			setLoggedIn(!!user);
+			setUser(user);
+		});
+	}
+
+	let logout = () => {
+		netlifyAuth.signout(() => {
+			setLoggedIn(false);
+			setUser(null);
+		});
+	};
+
+	return (
+		<div className="container">
+		<Head>
+			<title>Next.js Starter!</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+
+		<main>
+			<Header title="Welcome to my app!" />
+			{loggedIn ?
+				<div>Logged in!</div> :
+				<button onClick={login}>Log In</button>
+			}
+			<p className="description">
+			Get started by editing <code>pages/index.js</code>
+			</p>
+		</main>
+
+		<Footer />
+		</div>
+	)
 }
